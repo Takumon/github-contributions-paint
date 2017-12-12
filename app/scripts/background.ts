3b139786e300107327378e4609791a1c09d844ea
takumon <inouetakumon@gmail.com>
2017-12-12 22:52:50 +0900

    画面上で絵がかけるようにした（Git連携などはまだ）

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,8 +1,24 @@
-// Enable chromereload by uncommenting this line:
-// import 'chromereload/devonly'
+// GithubのContributesパネルの1マスをクリックした場合
 
-chrome.runtime.onInstalled.addListener((details) => {
-  console.log('previousVersion', details.previousVersion);
-});
+  // 1マス1マスに対してクリック時に色を変える
+  const $contributionPanel = <Element>document.querySelector('.js-calendar-graph-svg');
+  if ($contributionPanel) {
+    // mouseenterをデリゲートしたいのでキャプチャフェーズでイベントを監視する
+    $contributionPanel.addEventListener('mouseenter', setColor, true);
+  }
 
-console.log(`'Allo 'Allo! Event Page`);
+  // 色をセットする
+  function setColor(event: any) {
+    if (!event.shiftKey) return;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    // 1マスの要素fillに対して色を設定する
+    chrome.storage.sync.get('curColor', item => {
+      const target = <HTMLElement> event.target;
+      target.setAttribute('fill', item.curColor);
+    });
+    return false;
+  }
+
---
+++ app/scripts/popup.ts
@@ -0,0 +1,50 @@
+/**
+ * 初期ロード時の処理
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  // corColorが変わった時に、入力項目に反映させる
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    chrome.storage.sync.get('curColor', function(item) {
+        const $colorInput = <HTMLInputElement>document.getElementById('colorInput');
+        if ($colorInput) {
+          $colorInput.value = item.curColor;
+        }
+    });
+  });
+
+
+  // 入力項目に色が指定された場合にそれをcurColorに設定する
+  const $colorInput = <HTMLInputElement>document.getElementById('colorInput');
+  $colorInput.addEventListener('input', function() {
+    changeColor($colorInput.value);
+  });
+
+  // curColorを入力項目の値に初期値として設定する
+  chrome.storage.sync.get('curColor', (item) => {
+      const $colorInput = <HTMLInputElement>document.getElementById('colorInput');
+      $colorInput.value = item.curColor;
+  });
+
+  const $colors = document.querySelectorAll('.color');
+  for (let i = 0; i < $colors.length; i++) {
+      $colors[i].addEventListener('click', onClick);
+  }
+});
+
+// Colorがクリック時の処理
+function onClick(event: Event) {
+  const target = <HTMLElement> event.target;
+  changeColor(target.getAttribute('data-id'));
+}
+
+
+/**
+ * 色変更
+ *
+ * @param color 色
+ */
+function changeColor(color: string | null) {
+  if (!color) return;
+
+  chrome.storage.sync.set({ 'curColor': color }, () => console.log('Color changed'));
+}
